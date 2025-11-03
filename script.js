b0a2ae3d89818697e8bca535af47750327ce33f2
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-11-04 02:07:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const secondHand = document.querySelector(".second-hand");
 
 setInterval(()=> {
 	const now = new Date()
-	const hour = 30*now.getHours() + 90;
+	const hour = (30*now.getHours() + 0.5*now.getMinutes()) + 90;
 	const min = 6*now.getMinutes() + 90;
 	const sec = 6*now.getSeconds() + 90;
 	console.log(hour)
